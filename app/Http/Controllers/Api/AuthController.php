72c1422ee74de0f73c34705f7e67f962c1069915
havidw <havidw2@gmail.com>
2025-06-26 12:20:36 +0530

    Implement AuthController for user authentication and update API routes

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Handle an authentication attempt.
+     */
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return response()->json([
+                'message' => 'Login successful',
+                'user' => Auth::user(),
+            ]);
+        }
+
+        return response()->json([
+            'message' => 'Login failed',
+        ], 401);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,26 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::post('/login', [AuthController::class, 'authenticate'])
+    ->middleware('web');
+
+Route::middleware('web', 'auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::get('/dashboard', function () {
+        return 'Welcome to Dash';
+    });
+
+});
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('web', 'auth:sanctum');
