63457e0286a2ad0e3a3ddef4cda932f89c072313
havidw <havidw2@gmail.com>
2025-07-07 08:24:47 +0530

    Add models and relationships for Exam, Organization, Student, OrgAdmin, ExamDate, and StudentExam

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Exam extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function organization()
+    {
+        return $this->belongsTo(Organization::class);
+    }
+
+    public function dates()
+    {
+        return $this->hasMany(ExamDate::class);
+    }
+
+    public function studentExams()
+    {
+        return $this->hasMany(StudentExam::class);
+    }
 }
--- app/Models/Organization.php
+++ app/Models/Organization.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Organization extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function students()
+    {
+        return $this->hasMany(Student::class);
+    }
+
+    public function exams()
+    {
+        return $this->hasMany(Exam::class);
+    }
+
+    public function orgAdmins()
+    {
+        return $this->hasMany(OrgAdmin::class);
+    }
 }
