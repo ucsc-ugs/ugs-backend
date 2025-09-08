b0c74b730dce350f8cbe0f94bae32c4f51524b70
Ranuga Lecamwasam <ranugalecamwasam2002@gmail.com>
2025-09-08 23:34:32 +0530

    students able to see registered exams

---
+++ app/Http/Resources/StudentExamResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Http\Request;
+
+class StudentExamResource extends JsonResource
+{
+    
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'index_number' => $this->index_number,
+            'student_id' => $this->student_id,
+            'exam_id' => $this->exam_id,
+            'payment_id' => $this->payment_id,
+            'status' => $this->status,
+            'attended' => (bool) $this->attended,
+            'result' => $this->result,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'exam' => new ExamResource($this->whenLoaded('exam')),
+            'payment' => new PaymentResource($this->whenLoaded('payment')),
+        ];
+    }
+}
